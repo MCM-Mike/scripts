612a8044e14e2f31cd0e2a341196008e54300182
MCM-Mike <m.c.mauer@gmail.com>
2019-01-23 21:24:48 +0100

    Update docker-lab-virtualbox.sh

--- docker-lab-virtualbox.sh
+++ docker-lab-virtualbox.sh
@@ -1,15 +1,27 @@
 # docker lab 
 
+#VirtualBox Virtual Machines
+#	VM Name	Role	OS	CPU Cores	RAM (MB)	Disk (GB)	Comments
+#1	rs	Rancher server	CoreOS	2	1024	10	
+#VirtualBox → VM Settings → Network → Adapter 1: NAT
+#VirtualBox → VM Settings → Network → Adapter 2: Bridged Adapter
+#2	ra1	Rancher agent	2048	s20
+#3	ra2	Rancher agent	2048	20Hard disk file type: VDI (VirtualBox Disk Image)
+#Storage on physical hard disk: Dynamically allocated
+# source: https://github.com/satrapu/rancher-workshop/wiki/VirtualBox
+
+
+#my scripts
 # local execution on workstation
 sudo docker run -d --restart=unless-stopped -p 80:80 -p 443:443 rancher/rancher:stable
 
 # start vms in virtualbox
 # check new version URL https://github.com/rancher/os/releases 
 
-docker-machine create -d virtualbox --virtualbox-boot2docker-url https://github.com/rancher/os/releases/download/v1.5.0/rancheros.iso rancheros1
-docker-machine create -d virtualbox --virtualbox-boot2docker-url https://github.com/rancher/os/releases/download/v1.5.0/rancheros.iso rancheros2
-docker-machine create -d virtualbox --virtualbox-boot2docker-url https://github.com/rancher/os/releases/download/v1.5.0/rancheros.iso rancheros3
-docker-machine create -d virtualbox --virtualbox-boot2docker-url https://github.com/rancher/os/releases/download/v1.5.0/rancheros.iso rancheros4
+docker-machine create -d virtualbox --virtualbox-boot2docker-url https://releases.rancher.com/os/latest/rancheros.iso  rancheros1
+docker-machine create -d virtualbox --virtualbox-boot2docker-url https://releases.rancher.com/os/latest/rancheros.iso  rancheros2
+docker-machine create -d virtualbox --virtualbox-boot2docker-url https://releases.rancher.com/os/latest/rancheros.iso  rancheros3
+docker-machine create -d virtualbox --virtualbox-boot2docker-url https://releases.rancher.com/os/latest/rancheros.iso  rancheros4
 
 # check network in virtualbox 
 # bridged for local access or setup new net in virtualbox
